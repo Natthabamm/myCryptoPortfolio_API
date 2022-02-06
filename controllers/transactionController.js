e3bf5c1a8eaaa269bb713634c7b9a62be461cd07
natthabam <natthamonbam@gmail.com>
2022-02-06 21:50:42 +0700

    add updateTransaction completed

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -1,32 +1,29 @@
-const fa = require("fs");
 const { Op } = require("sequelize");
 const { Transaction, User, sequelize } = require("../models");
 
-exports.createTransaction = async (req, res, next) => {
+exports.getAllTransactions = async (req, res, next) => {
   try {
-    const { transactionType, coinName, quanity, pricePerCoin, date, time, totalSpent, currency  } =
-      req.body;
-      console.log(req.body)
-    if (!transactionType && !coinName && !quanity && !pricePerCoin  && !date && !time && !totalSpent && !currency ) {
-      return res
-        .status(400)
-        .json({
-          message:
-            "transaction type or coin name or quanity or datetime or totalspent is required",
-        });
-    };
+    const transactions = await Transaction.findAll({
+      where: { userId: req.user.id },
+      order: [["date", "DESC"]],
+    });
+    if (!transactions) {
+      return res.status(404).json({ message: "transaction not found" });
+    }
 
-    const transaction = await Transaction.create({
-        userId: req.user.id,
-        transactionType,
-        coinName,
-        quanity,
-        pricePerCoin,
-        date,
-        time,
-        totalSpent,
-        currency
-    })
+    res.status(200).json({ transactions });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getTransactionById = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const transaction = await Transaction.findOne({ where: { id } });
+    if (!transaction) {
+      return res.status(400).json({ message: "transaction not found" });
+    }
 
     res.status(201).json({ transaction });
   } catch (err) {
@@ -34,4 +31,97 @@ exports.createTransaction = async (req, res, next) => {
   }
 };
 
+exports.createTransaction = async (req, res, next) => {
+  try {
+    const {
+      transactionType,
+      coinName,
+      quanity,
+      pricePerCoin,
+      date,
+      time,
+      totalSpent,
+      currency
+    } = req.body;
 
+    if (
+      !transactionType &&
+      !coinName &&
+      !quanity &&
+      !pricePerCoin &&
+      !date &&
+      !time &&
+      !totalSpent &&
+      !currency
+    ) {
+      return res.status(400).json({
+        message:
+          "transaction type or coin name or quanity or datetime or totalspent is required",
+      });
+    }
+
+    const transaction = await Transaction.create({
+      userId: req.user.id,
+      transactionType,
+      coinName,
+      quanity,
+      pricePerCoin,
+      date,
+      time,
+      totalSpent,
+      currency,
+    });
+
+    res.status(201).json({ transaction });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.updateTransaction = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { 
+      quanity,
+      pricePerCoin,
+      date,
+      time,
+      totalSpent
+    } = req.body;
+    
+    const transaction = await Transaction.update({ 
+      quanity,
+      pricePerCoin,
+      date,
+      time,
+      totalSpent }, {
+      where: {
+        [Op.and]: [{ id }, { userId: req.user.id }],
+      },
+    });
+
+
+    res.status(200).json({ message: 'update transaction completed', transaction });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.deleteTransaction = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const transaction = await Transaction.findOne({
+      where: {
+        [Op.and]: [{ id }, { userId: req.user.id }],
+      },
+    });
+    if (!transaction) {
+      return res.status(404).json({ message: "transaction not found" });
+    }
+    await transaction.destroy();
+
+    res.status(204).json({ message: "delete transaction completed" });
+  } catch (err) {
+    next(err);
+  }
+};
--- routes/transactionRoute.js
+++ routes/transactionRoute.js
@@ -4,6 +4,10 @@ const transactionController = require('../controllers/transactionController');
 
 const router = express.Router();
 
+router.get('/:id', authenticate, transactionController.getTransactionById);
+router.get('/', authenticate, transactionController.getAllTransactions);
 router.post('/', authenticate, transactionController.createTransaction);
+router.put('/:id', authenticate, transactionController.updateTransaction);
+router.delete('/:id', authenticate, transactionController.deleteTransaction);
 
 module.exports = router;
